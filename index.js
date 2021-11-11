c192174959f087e8b9c0ce24c7121ba96a3d06a2
Rabi-Mezan <ravimezan@gmail.com>
2021-11-11 19:11:24 +0600

    admin functionality added

--- index.js
+++ index.js
@@ -23,12 +23,51 @@ async function run() {
 
         const database = client.db("flashCorner");
         const productsCollection = database.collection("products");
+        const usersCollection = database.collection("users")
 
-
+        // add products api
         app.post('/products', async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product)
             res.json(result);
+        })
+
+        // get products api
+        app.get('/products', async (req, res) => {
+            const result = await productsCollection.find({}).toArray()
+            res.json(result)
+        })
+
+        // post api for users
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+
+        //make admin api
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user.email }
+            const updateDoc = { $set: { role: 'admin' } }
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.json(result)
+        })
+
+
+        // admin cheking api
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin })
         })
 
     }
